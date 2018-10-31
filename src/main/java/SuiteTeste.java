2799e089c888cc26680661b7deac27b62f583811
David Batista de Luna <37220913+DavidLuna0@users.noreply.github.com>
2018-10-31 11:27:41 -0300

    Add files via upload
    First commit

---
+++ src/main/java/SuiteTeste.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+//suite dos testes
+@RunWith(Suite.class)
+@SuiteClasses({ TesteCadastro.class, TesteRegrasCadastro.class, TesteCampoTreinamento.class })
+public class SuiteTeste {
+
+}
